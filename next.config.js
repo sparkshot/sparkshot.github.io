2615243ba7dba43d6b2cc2c9a933673fa1fe9c8d
Drew Lee <drewlee@rightpoint.com>
2022-08-03 13:53:33 -0400

    init commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    loader: "akamai",
+    path: " ",
+  },
+  // basePath: "/home",
+  // assetPrefix: "/home",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

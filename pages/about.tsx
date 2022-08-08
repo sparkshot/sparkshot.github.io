1a0a55d00e94bfb41416e58fdcea4249a2fd5dcc
Drew Lee <drewlee@rightpoint.com>
2022-08-08 13:04:29 -0400

    glow

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,28 +13,21 @@ const About: NextPage = () => {
           rel="icon"
           href={`${process.env.NEXT_PUBLIC_FAVICON}icons8-camera-snap.gif`}
         />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Sacramento&display=swap"
+        />
       </Head>
 
       <main className={styles.main}>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>HELLO this is about page &rarr;</h2>
-          </a>
+          <h1 className={styles.name}>
+            <em>Sparkshot Media</em>
+          </h1>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 };

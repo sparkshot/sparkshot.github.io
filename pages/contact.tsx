9c20486c697753816dbc62a7e716cc78cc972f76
Drew Lee <drewlee@rightpoint.com>
2022-08-10 13:35:15 -0400

    emoji no work :(

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,15 +7,15 @@ const Contact: NextPage = () => {
       <h1>
         If you're a hot girl, hit me up. I wear an upside down sweater when I'm
         chilling at my crib{" "}
-        <span role="img" aria-label="face">
+        {/* <span role="img" aria-label="face">
           😏
-        </span>
+        </span> */}
         <p className={styles.textString}>
           How will we meet you ask? Just yell really loud when you get to
           woodside station. I'll come get u{" "}
-          <span role="img" aria-label="sheep">
+          {/* <span role="img" aria-label="sheep">
             🐑
-          </span>
+          </span> */}
         </p>
       </h1>
     </div>

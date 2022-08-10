620f70b80d5b28ec9539d5e621f41eece9787518
Drew Lee <drewlee@rightpoint.com>
2022-08-10 13:32:47 -0400

    emoji fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,12 +6,18 @@ const Contact: NextPage = () => {
     <div className={styles.container}>
       <h1>
         If you're a hot girl, hit me up. I wear an upside down sweater when I'm
-        chilling at my crib 😏
+        chilling at my crib{" "}
+        <span role="img" aria-label="face">
+          😏
+        </span>
+        <p className={styles.textString}>
+          How will we meet you ask? Just yell really loud when you get to
+          woodside station. I'll come get u{" "}
+          <span role="img" aria-label="sheep">
+            🐑
+          </span>
+        </p>
       </h1>
-      <p>
-        How will we meet you ask? Just yell really loud when you get to woodside
-        station. I'll come get yer booty.
-      </p>
     </div>
   );
 };

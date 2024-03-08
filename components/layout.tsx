a63e7b5b83332929cc2cd9ec76ab2d563a4f5762
andrewhylee <drewlee@rightpoint.com>
2024-03-08 15:47:10 -0500

    fix 2

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
 
 interface Props {
   children?: ReactNode;

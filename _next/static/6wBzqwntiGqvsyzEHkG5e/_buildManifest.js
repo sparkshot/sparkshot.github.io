c6f1cc655ed96a645de033a6d217864b3a6bc234
sparkshot <110427878+sparkshot@users.noreply.github.com>
2026-08-20 17:19:10 +0000

    Deploying to gh-pages from @ sparkshot/sparkshot.github.io@5b9599dc174f7c0c05f5968dbe79e0b730c08cb7 🚀

---
+++ _next/static/6wBzqwntiGqvsyzEHkG5e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/819-40ddc28a541e8f57.js","static/css/f43c8189779bc7c9.css","static/chunks/pages/index-de7c1ad411c20787.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
